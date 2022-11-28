bf04c62a62117445d0b6ffe44d3e319630f16139
Stuart Preston <mail@stuartpreston.net>
2022-11-28 12:34:30 +0000

    Updating cloud config
    Signed-off-by: Stuart Preston <mail@stuartpreston.net>

--- azure/preconfigure.sh
+++ azure/preconfigure.sh
@@ -13,12 +13,15 @@ if [[ ! -f /.kubelift ]] ; then
 fi
 EOF
 
-cat <<EOF | sudo tee /var/lib/cloud/scripts/per-instance/instance.sh
+cat <<EOF | sudo tee /var/lib/cloud/scripts/vendor/instance.sh
 #!/bin/sh 
 sudo sh /var/lib/polverio/activate.sh
 EOF
 
-chmod 0755 /var/lib/cloud/scripts/per-instance/instance.sh
+chmod 0755 /var/lib/cloud/scripts/vendor/instance.sh
+
+# move scripts-vendor into the init phase when networking is present
+cat /var/cloud/cloud.cfg | sed "s/-\ ssh$/-\ ssh\\n\ -\ scripts-vendor/g" | sudo tee /var/cloud/cloud.cfg
 
 # generalize the image
 # sudo waagent -deprovision+user --force
