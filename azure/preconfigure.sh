1f8d693029c36401d2b04bb0a141ad0d474491ae
Stuart Preston <mail@stuartpreston.net>
2022-11-28 12:49:49 +0000

    Testing per-instance cloud meta
    Signed-off-by: Stuart Preston <mail@stuartpreston.net>

--- azure/preconfigure.sh
+++ azure/preconfigure.sh
@@ -13,15 +13,15 @@ if [[ ! -f /.kubelift ]] ; then
 fi
 EOF
 
-cat <<EOF | sudo tee /var/lib/cloud/scripts/vendor/instance.sh
+cat <<EOF | sudo tee /var/lib/cloud/scripts/per-instance/instance.sh
 #!/bin/sh 
 sudo sh /var/lib/polverio/activate.sh
 EOF
 
-chmod 0755 /var/lib/cloud/scripts/vendor/instance.sh
+chmod 0755 /var/lib/cloud/scripts/per-instance/instance.sh
 
-# move scripts-vendor into the init phase when networking is present
-cat /etc/cloud/cloud.cfg | sed "s/-\ ssh$/-\ ssh\\n\ -\ scripts-vendor/g" | sudo tee /etc/cloud/cloud.cfg
+# move scripts-per-instance into the init phase when networking is present
+cat /etc/cloud/cloud.cfg | sed "s/-\ ssh$/-\ ssh\\n\ -\ scripts-per-instance/g" | sudo tee /etc/cloud/cloud.cfg
 
 # generalize the image
 # sudo waagent -deprovision+user --force
